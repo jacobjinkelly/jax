e21515b3e48ef2aee4e606be96fe004d38ec18e5
Jacob Kelly <jacob.jin.kelly@gmail.com>
2019-09-11 15:13:15 -0400

    training works

--- examples/neural_odes.py
+++ examples/neural_odes.py
@@ -10,7 +10,7 @@ import argparse
 from jax.experimental import stax
 from jax.experimental.stax import Dense, Tanh
 from jax.experimental.ode import odeint, grad_odeint
-from jax import random
+from jax import random, grad
 import jax.numpy as np
 from jax.config import config
 from jax.flatten_util import ravel_pytree
@@ -21,7 +21,8 @@ parser.add_argument('--data_size', type=int, default=1000)
 parser.add_argument('--batch_time', type=int, default=10)
 parser.add_argument('--batch_size', type=int, default=20)
 parser.add_argument('--niters', type=int, default=2000)
-parser.add_argument('--test_freq', type=int, default=20)
+parser.add_argument('--lr', type=float, default=0.001)
+parser.add_argument('--test_freq', type=int, default=1)
 parser.add_argument('--viz', action='store_true')
 parser.add_argument('--gpu', type=int, default=0)
 parser.add_argument('--adjoint', action='store_true')
@@ -61,14 +62,6 @@ def get_batch():
     return batch_y0, batch_t, batch_y
 
 
-def visualize(true_y, pred_y, odefunc, itr):
-    """
-    Visualize the model's predictions.
-    """
-    # TODO
-    pass
-
-
 if __name__ == "__main__":
 
     ii = 0
@@ -103,25 +96,29 @@ if __name__ == "__main__":
         flat_predictions, _ = ravel_pytree(predictions)
         return flat_predictions
 
+    def loss_fun(pred, target):
+        """
+        Loss function.
+        """
+        return np.mean(np.abs(pred - target))
+
+    grad_loss_fun = grad(loss_fun)
+
     for itr in range(1, args.niters + 1):
         batch_y0, batch_t, batch_y = get_batch()
         flat_batch_y0, _ = ravel_pytree(batch_y0)
         pred_y = odeint(f, fargs, flat_batch_y0, batch_t, atol=1e-8, rtol=1e-8)
-        pred_y = ravel_batch_y(pred_y)
-        loss = np.mean(np.abs(pred_y - batch_y))
 
-        # TODO: one step of SGD
+        _, ravel_pred_y = ravel_pytree(pred_y)
 
-        # t0 = 0.1
-        # t1 = 0.2
-        # y0 = np.linspace(0.1, 0.9, dim)
-        # ys = odeint(f, fargs, y0, np.array([t0, t1]), atol=1e-8, rtol=1e-8)
-        # ode_vjp = grad_odeint(f, fargs)
-        # g = np.ones_like(ys)
-        # exact_grad, _ = ravel_pytree(ode_vjp(g, ys, np.array([t0, t1])))
+        ode_vjp = grad_odeint(f, fargs)
+        loss_grad = grad_loss_fun(ravel_batch_y(pred_y), batch_y)
+        total_grad = ode_vjp(ravel_pred_y(loss_grad), pred_y, batch_t)
+
+        params_grad = total_grad[3]
+        fargs -= args.lr * params_grad
 
         if itr % args.test_freq == 0:
-            # print('Iter {:04d} | Total Loss {:.6f}'.format(itr, loss))
-            # visualize(true_y, pred_y, f, ii)
-            print(ii)
+            loss = loss_fun(ravel_batch_y(pred_y), batch_y)
+            print('Iter {:04d} | Total Loss {:.6f}'.format(itr, loss))
             ii += 1
