3a6f217624c7254a879e96e33ec72713492a95d2
Jacob Kelly <jacob.jin.kelly@gmail.com>
2019-09-11 16:55:23 -0400

    implemented regularization

--- examples/neural_odes.py
+++ examples/neural_odes.py
@@ -22,6 +22,7 @@ parser.add_argument('--batch_time', type=int, default=10)
 parser.add_argument('--batch_size', type=int, default=20)
 parser.add_argument('--niters', type=int, default=2000)
 parser.add_argument('--lr', type=float, default=0.001)
+parser.add_argument('--lam', type=float, default=1)
 parser.add_argument('--test_freq', type=int, default=1)
 parser.add_argument('--viz', action='store_true')
 parser.add_argument('--gpu', type=int, default=0)
@@ -77,7 +78,17 @@ if __name__ == "__main__":
 
     flat_params, ravel_params = ravel_pytree(init_params)
     batch_y0, _, batch_y = get_batch()
+
+    r0 = np.zeros((args.batch_size, 1))
+    batch_y0_r0 = np.concatenate((batch_y0, r0), axis=1)
+    _, ravel_batch_y0_r0 = ravel_pytree(batch_y0_r0)
+
     _, ravel_batch_y0 = ravel_pytree(batch_y0)
+
+    r = np.zeros((args.batch_time, args.batch_size, 1))
+    batch_y_r = np.concatenate((batch_y, r), axis=2)
+    _, ravel_batch_y_r = ravel_pytree(batch_y_r)
+
     _, ravel_batch_y = ravel_pytree(batch_y)
     fargs = flat_params
 
@@ -96,29 +107,60 @@ if __name__ == "__main__":
         flat_predictions, _ = ravel_pytree(predictions)
         return flat_predictions
 
-    def loss_fun(pred, target):
+    def reg_dynamics(y_r, t, *args):
+        """
+        Augmented dynamics to implement regularization.
+        """
+        flat_params = args
+        # convert flat_params from Tuple to DeviceArray, then ravel
+        flat_params, _ = ravel_pytree(flat_params)
+        params = ravel_params(flat_params)
+
+        # separate out state from augmented
+        y_r = ravel_batch_y0_r0(y_r)
+        # TODO: do we need to use jax.index here?
+        y, r = y_r[:, :2], y_r[:, 2]
+
+        predictions = predict(params, y ** 3)
+        # STATE
+        regularization = np.sum(y ** 2, axis=1) ** 0.5
+        # DYNAMICS
+        regularization = np.sum(predictions ** 2, axis=1) ** 0.5
+
+        regularization = np.expand_dims(regularization, axis=1)
+        pred_reg = np.concatenate((predictions, regularization), axis=1)
+        flat_pred_reg, _ = ravel_pytree(pred_reg)
+        return flat_pred_reg
+
+    def loss_fun(pred_y_r, target):
         """
         Loss function.
         """
-        return np.mean(np.abs(pred - target))
+        pred, reg = pred_y_r[:, :, :2], pred_y_r[:, :, 2]
+        # TODO: not making a silly math mistake here right?
+        return np.mean(np.abs(pred - target)) + args.lam * np.mean(reg)
 
     grad_loss_fun = grad(loss_fun)
 
     for itr in range(1, args.niters + 1):
         batch_y0, batch_t, batch_y = get_batch()
-        flat_batch_y0, _ = ravel_pytree(batch_y0)
-        pred_y = odeint(f, fargs, flat_batch_y0, batch_t, atol=1e-8, rtol=1e-8)
+        r0 = np.zeros((args.batch_size, 1))
+        batch_y0_r0 = np.concatenate((batch_y0, r0), axis=1)
+        flat_batch_y0_r0, _ = ravel_pytree(batch_y0_r0)
+        pred_y_r = odeint(reg_dynamics, fargs, flat_batch_y0_r0, batch_t, atol=1e-8, rtol=1e-8)
 
-        _, ravel_pred_y = ravel_pytree(pred_y)
+        _, ravel_pred_y_r = ravel_pytree(pred_y_r)
 
-        ode_vjp = grad_odeint(f, fargs)
-        loss_grad = grad_loss_fun(ravel_batch_y(pred_y), batch_y)
-        total_grad = ode_vjp(ravel_pred_y(loss_grad), pred_y, batch_t)
+        ode_vjp = grad_odeint(reg_dynamics, fargs)
+        loss_grad = grad_loss_fun(ravel_batch_y_r(pred_y_r), batch_y)
+
+        total_grad = ode_vjp(ravel_pred_y_r(loss_grad), pred_y_r, batch_t)
 
         params_grad = total_grad[3]
         fargs -= args.lr * params_grad
 
         if itr % args.test_freq == 0:
+            pred_y = ravel_batch_y_r(pred_y_r)[:, :, :2]
             loss = loss_fun(ravel_batch_y(pred_y), batch_y)
             print('Iter {:04d} | Total Loss {:.6f}'.format(itr, loss))
             ii += 1
