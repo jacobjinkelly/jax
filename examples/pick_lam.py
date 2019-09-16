07a0816732f69736776c8e23be8cdd62c3a49728
Jacob Kelly <jacob.jin.kelly@gmail.com>
2019-09-16 16:57:55 -0400

    add code for picking scale of lam

---
+++ examples/pick_lam.py
@@ -0,0 +1,40 @@
+"""
+For parsing and plotting the results of neural_odes.py
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+dir = "2019-09-16-16-12-07"
+results_path = "%s/results.txt" % dir
+
+file = open(results_path, "r")
+
+lines = file.readlines()
+
+inds = []
+for i, line in enumerate(lines):
+    if line.startswith("Reg:"):
+        inds.append(i)
+
+losses = {}
+for ind, next_ind in zip(inds, inds[1:] + [len(lines)]):
+    split = lines[ind][len("Reg: "):].split("Lambda ")
+    reg_type, lam = split[0][:-1], float(split[1][:-1])
+    if reg_type == "none":
+        continue
+    if reg_type not in losses:
+        losses[reg_type] = []
+
+    data = lines[ind + 1:next_ind]
+    loss_lines = []
+    for i, line in enumerate(data):
+        if line.startswith("Iter"):
+            loss_lines.append(line)
+
+    for line in loss_lines:
+        split = line.split(" | ")
+        loss, reg = float(split[-2].split("Loss ")[-1][:-1]), float(split[-1].split("Regularization ")[-1][:-1])
+        losses[reg_type].append(loss / reg)
+
+for reg in losses.keys():
+    print("Reg: %s, Loss/Reg: %.3f" % (reg, np.mean(losses[reg])))
