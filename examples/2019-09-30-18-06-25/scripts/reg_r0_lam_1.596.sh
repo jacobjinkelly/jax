9f357229273f0ea709ac6e0226e0910b6281fb4f
jkelly <Jacob Kelly>
2019-10-01 09:49:56 -0400

    add results

---
+++ examples/2019-09-30-18-06-25/scripts/reg_r0_lam_1.596.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python neural_odes_demo2.py --reg=r0 --lam=1.596 --dirname=/h/jkelly/jax/examples/2019-09-30-18-06-25 > /h/jkelly/jax/examples/2019-09-30-18-06-25/reg_r0_lam_1.596.o 2> /h/jkelly/jax/examples/2019-09-30-18-06-25/reg_r0_lam_1.596.e
---
+++ examples/2019-09-30-18-06-25/scripts/reg_r1_lam_76.880.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python neural_odes_demo2.py --reg=r1 --lam=76.880 --dirname=/h/jkelly/jax/examples/2019-09-30-18-06-25 > /h/jkelly/jax/examples/2019-09-30-18-06-25/reg_r1_lam_76.880.o 2> /h/jkelly/jax/examples/2019-09-30-18-06-25/reg_r1_lam_76.880.e
