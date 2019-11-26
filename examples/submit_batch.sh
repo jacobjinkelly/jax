12494ec0754486200a430031ba47f92f21f9878c
jkelly <Jacob Kelly>
2019-11-26 10:41:29 -0500

    save configs

--- examples/submit_batch.sh
+++ examples/submit_batch.sh
@@ -7,6 +7,7 @@ mem=$3
 # activate environment
 . ../../jax_nodes.env
 
+# set up directory to store results
 curr_time=$(date "+%F-%H-%M-%S")
 scripts=$(pwd)/$curr_time/scripts
 results=$(pwd)/$curr_time
@@ -16,13 +17,19 @@ mkdir -p $results
 args="$file $node $mem $scripts $results"
 
 r0_lam_file="r0_lams.txt"
+r1_lam_file="r1_lams.txt"
+
+# save config
+cp $file $results
+cp $r0_lam_file $results
+cp $r1_lam_file $results
+cat $args > $results/args.txt
 
 while IFS= read -r line
 do
     ./submit.sh $args r0 $line
 done < "$r0_lam_file"
 
-r1_lam_file="r1_lams.txt"
 
 while IFS= read -r line
 do
